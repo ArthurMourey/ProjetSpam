6478ac9ac6e2846256244db5048dad7e2f66d143
MarintheJerome <jerome.marinthe@laposte.net>
2016-12-12 10:26:19 +0100

    changement traitement cluster + modèle de tests plus gros

--- src/main/scala/EmailClassifier.scala
+++ src/main/scala/EmailClassifier.scala
@@ -1,87 +1,115 @@
-import java.io.File
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, LogisticRegressionWithSGD}
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
 import org.apache.spark.rdd.RDD
-import scala.collection.mutable.ListBuffer
 
-object EmailClassifier extends App {
-    val conf = new SparkConf().setAppName("email-spam").setMaster("local[*]")
-    val sc = new SparkContext(conf)
-    val spam = sc.textFile("./donneesModele/nonSpam/*", 4)
-    val normal = sc.textFile("./donneesModele/spam/*", 4)
-    val tf = new HashingTF(numFeatures = 10000)
-    val spamFeatures = spam.map(email => tf.transform(email.split(" ")))
-    val normalFeatures = normal.map(email => tf.transform(email.split(" ")))
-    val positiveExamples = spamFeatures.map(features => LabeledPoint(1, features))
-    val negativeExamples = normalFeatures.map(features => LabeledPoint(0, features))
-    val trainingData = positiveExamples.union(negativeExamples)
-    trainingData.cache()
-    val model = new LogisticRegressionWithLBFGS().run(trainingData)
+object EmailClassifier {
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf().setAppName("email-spam")
+        if(args(0).equals("local")) {
+            // Création du context spark
+            val sc = new SparkContext(conf)
 
-    val mails = sc.wholeTextFiles("./maildir/*/*/*")
+            // On récupère les données pour créer le modèle
+            // Crée un RDD avec un mail par ligne
+            // Nb de partitions = nb de mails qu'on a mit dans les dossiers
+            val valide = sc.wholeTextFiles("donneesModele/nonSpam/*")
+            val spam = sc.wholeTextFiles("donneesModele/spam/*")
 
-   /* //Test on a positive example (spam) and a negative one (normal).
-    val posTest = tf.transform("remembd that sharks ass with the  billy club?".split(" "))
-    val negTest = tf.transform("hi sorry yaar i forget tell you i cant come today".split(" "))*/
+            val tf = new HashingTF(10000)
 
-    val res = mails.map(email => {
-        val t = model.predict(tf.transform(email._2.split(" ")))
-        if(t ==1){
-            println("SPAM :"+email._1)
-        }else{
-            println("NON SPAM : "+email._1)
+            // Retourne un autre RDD en transformant spam en vecteur de fréquence qui permet d'avoir la fréquence d'un mot
+            val caracteristiquesSpam = spam.map(email => tf.transform(email._2.split(" ")))
+            val caracteristiquesNonSpam = valide.map(email => tf.transform(email._2.split(" ")))
+
+
+            // Retourne un autre RDD en utilisant les vecteurs LabeledPoints (1 pour spam, 0 pour non spam)
+            // On fait l'union des deux RDD contenant les spams et les non spams
+            val donnesModele = caracteristiquesSpam.map(carac => LabeledPoint(1, carac))
+              .union(caracteristiquesNonSpam.map(carac => LabeledPoint(0, carac)))
+            // On cache les données pour que l'exécution soit plus rapide
+            donnesModele.cache()
+
+            // On crée le modele
+            val modele = new LogisticRegressionWithLBFGS().run(donnesModele)
+
+            // On obtient les mails qui vont être appliqués au model
+            val mails = getAllMails(sc) // mail._1 = nom du mail, mail._2 contenu du mail
+            val nbMails = mails.count()
+
+            // On parcourt les mails et on regarde s'ils sont spam ou non spam
+            // Si le modèle renvoie 1 c'est un spam, sinon 0
+            val res = mails.map(mail => {
+                val result = modele.predict(tf.transform(mail._2.split(" ")))
+                if (result == 1) {
+                    println("Spam : " + mail._1)
+                }
+                if(result == 0){
+                    println("valide : " + mail._1)
+                }
+                result
+            }).reduce((val1, val2) => val1 + val2)
+            Thread.sleep(5000);
+            printResults(spam, valide, nbMails, res)
         }
-        t
-    }).reduce((val1, val2) => val1 + val2)
-    println("Nb de spams : "+res)
-    println("Pourcentage spams : "+(res/mails.count()*100))
-   /* println("Prediction for positive test example: " + model.predict(posTest))
-    println("Prediction for negative test example: " + model.predict(negTest))*/
+        else{ // on est sur le cluster, pas besoin de spark context
+            val sc = new SparkContext(conf)
+            val valide = sc.wholeTextFiles("hdfs:/user/marinthe/nonSpam/*", 4)
+            val spam = sc.wholeTextFiles("hdfs:/user/marinthe/spam/*", 4)
 
-    /*val conf = new SparkConf().setAppName("ProjetSpam").setMaster("local[*]")
-    val sc = new SparkContext(conf)
+            val tf = new HashingTF(numFeatures = 10000)
+            val caracteristiquesSpam = spam.map(email => tf.transform(email._2.split(" ")))
+            val caracteristiquesNonSpam = valide.map(email => tf.transform(email._2.split(" ")))
+            val positiveExamples = caracteristiquesSpam.map(features => LabeledPoint(1, features))
+            val negativeExamples = caracteristiquesNonSpam.map(features => LabeledPoint(0, features))
+            val trainingData = positiveExamples.union(negativeExamples)
+            trainingData.cache()
+            val model = new LogisticRegressionWithLBFGS().run(trainingData)
 
-    val tf = new HashingTF(numFeatures = 1000)
+            var nbSpams = 0d
+            var nbMails = 0
 
-    val spam = lireTousLesEmail("/donnees/modele/spam", estSpam = true)
-    val normal = lireTousLesEmail(s"/donnees/modele/nonSpam", estSpam = false)
+            for(i<- 1 to 11){ // Sur le cluster les fichiers sont dans 11 sours-repertoires
+                val mails = getAllMailsCluster(sc, i)
+                val res = mails.map(mail => {
+                    val result = new Tuple2[Double, Int](
+                        model.predict(tf.transform(mail._2.split(" "))), 1
+                    )
 
-    val trainingData = spam.union(normal)
-    trainingData.cache()
-
-    val model = new LogisticRegressionWithLBFGS().run(trainingData)
-
-    //Test on a positive example (spam) and a negative one (normal).
-    val posTest = tf.transform("insurance plan which change your life ...".split(" "))
-    val negTest = tf.transform("hi sorry yaar i forget tell you i cant come today".split(" "))
-
-    println("Prediction for positive test example: " + model.predict(posTest))
-    println("Prediction for negative test example: " + model.predict(negTest))
-
-    def lireTousLesEmail(cheminDossier: String, estSpam: Boolean): RDD[LabeledPoint] = {
-        var listeLabeledPoint: ListBuffer[LabeledPoint] = ListBuffer()
-        val dossier = new File(cheminDossier)
-
-        println(dossier)
-        for(fichier <- dossier.listFiles() if fichier.getName endsWith ".txt"){
-            var spam = -1
-            if(estSpam){
-                spam = 1;
+                    if (result._1 == 1) {
+                        println("Spam :" + mail._1)
+                    }
+                    if(result._1 == 0){
+                        println("valide : " + mail._1)
+                    }
+                    result
+                }).reduce((val1, val2) =>  (val1._1 + val2._1, val1._2 + val2._2 ))
+                nbSpams += res._1
+                nbMails += res._2
             }
-            else {
-                spam = 0;
-            }
+            //val nbMails = mails.count()
 
-            val spamFeatures = sc.textFile(fichier.getAbsolutePath).map(email => tf.transform(email.split(" ")))
-            val lPoint = new LabeledPoint(spam, tf.transform(spamFeatures.collect()))
-            listeLabeledPoint += lPoint
+
+            Thread.sleep(5000);
+            printResults(spam, valide, nbMails, nbSpams)
         }
-        val toReturn = sc.parallelize(listeLabeledPoint);
-        toReturn
-    }*/
+    }
+
+    def getAllMails(sc: SparkContext): RDD[(String, String)] = {
+        sc.wholeTextFiles("./maildir/*/*/*")
+    }
+
+    def getAllMailsCluster(sc: SparkContext, i: Int): RDD[(String, String)] ={
+        sc.wholeTextFiles("hdfs:/data/maildir"+i+"/*")
+    }
+
+    def printResults(spam : RDD[(String, String)], nonSpam : RDD[(String, String)], nbMails : Double, res : Double) = {
+        println("Nb de fichiers évalués : "+nbMails)
+        println("Nb de spams : "+res)
+        println("Pourcentage spams : "+(res/nbMails*100))
+    }
 }
 
